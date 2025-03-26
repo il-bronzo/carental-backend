597e3c75c58f29ee8fb27de7b65494544dd0fd04
Andrea Bronzoni <bronzoni.andrea@libero.it>
2025-03-26 15:06:31 +0100

    created server.js with the first setup

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const express = require ('express');
+const app = express();
+const PORT = process.env.PORT || 5005;
+
+app.get("/", (req, res) => {
+    res.send("Server is ready")
+})
+
+app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`))
